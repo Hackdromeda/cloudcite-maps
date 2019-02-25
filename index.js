d1fbfdb4a071d08bad0e4d1d439749db9be488ea
Naval Patel <navalpatel384@gmail.com>
2019-02-24 16:03:53 -0800

    updated typemap

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
 const fs = require('fs');
 const typeMap = require('./typeMap.json');
-
-function renameTypeMapKeys() {
-	let newTypeMap = {};
-	const types = Object.keys(typeMap);
-	for (let i = 0; i < types.length; i++) {
-		newTypeMap[types[i]] = [];
-		for (let j = 0; j < typeMap[types[i]].length; j++) {
-			if (typeMap[types[i]][j].csl && typeMap[types[i]][j].UI) {
-				newTypeMap[types[i]].push({"csl": typeMap[types[i]][j].csl, "displayText": typeMap[types[i]][j].UI})
-			}
-		}
+let keys = Object.keys(typeMap);
+/*
+for (let i=0;i<keys.length;i++) {
+	for (let j=0; j<typeMap[keys[i]].length; j++) {
+		typeMap[keys[i]][j].type = typeMap[keys[i]][j].csl;
+		typeMap[keys[i]][j] = {"csl": typeMap[keys[i]][j].csl, "displayText": typeMap[keys[i]][j].displayText, "group": typeMap[keys[i]][j].displayText}
 	}
-	fs.writeFile('newTypeMap.json', JSON.stringify(newTypeMap), (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
 }
-
+fs.writeFile('./typeMapNew.json', JSON.stringify(typeMap), err => {
+	console.log(err);
+})
+*/
 function splitTypeMap() {
-	const types = Object.keys(typeMap);
-	for (let i = 0; i < types.length; i++) {
-		fs.writeFile(`./fields/${types[i]}.json`, JSON.stringify(typeMap[types[i]]), (err) => {
+	for (let i=0;i<keys.length;i++) {
+		let fieldMap = typeMap[keys[i]].filter(typeArray => !typeArray.group && typeArray.csl != "" && typeArray.displayText != "");
+		let creatorsMap = typeMap[keys[i]].filter(typeArray => typeArray.group && typeArray.group === 'name-variables' && typeArray.csl != "" && typeArray.displayText != "");
+		for (let j=0;j<fieldMap.length;j++) {
+			fieldMap[j] = {"csl": fieldMap[j].csl, "displayText": fieldMap[j].displayText}
+		}
+		for (let j=0;j<creatorsMap.length;j++) {
+			creatorsMap[j] = {"csl": creatorsMap[j].csl, "displayText": creatorsMap[j].displayText}
+		}
+		fs.writeFile(`./fields/${keys[i]}.json`, JSON.stringify(fieldMap), (err) => {
+			if (err) {
+				console.log(err);
+			}
+		});
+		fs.writeFile(`./creators/${keys[i]}.json`, JSON.stringify(creatorsMap), (err) => {
 			if (err) {
 				console.log(err);
 			}
 		});
 	}
 }
+
+
+/*
+for (let i=0;i<keys.length;i++) {
+	fs.writeFile(`./fields/${keys[i]}.json`, JSON.stringify(typeMap[keys[i]]), (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
+*/
